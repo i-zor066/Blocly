b215c71189008e822b922d7202cfc91aa7c679ad
i-zor066 <igor.zor@gmail.com>
2015-08-30 15:46:37 +0200

    Checkpoint 55 completed.

--- Blocly/app/src/main/java/io/bloc/android/blocly/api/DataSource.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/api/DataSource.java
@@ -1,5 +1,7 @@
 package io.bloc.android.blocly.api;
 
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
 import java.text.DateFormat;
@@ -23,6 +25,9 @@ import io.bloc.android.blocly.api.network.GetFeedsNetworkRequest;
  * Created by igor on 13/8/15.
  */
 public class DataSource {
+
+    public static final String ACTION_DOWNLOAD_COMPLETED = DataSource.class.getCanonicalName().concat(".ACTION_DOWNLOAD_COMPLETED");
+
     private DatabaseOpenHelper databaseOpenHelper;
     private RssFeedTable rssFeedTable;
     private RssItemTable rssItemTable;
@@ -36,7 +41,6 @@ public class DataSource {
                 rssFeedTable, rssItemTable);
         feeds = new ArrayList<RssFeed>();
         items = new ArrayList<RssItem>();
-        createFakeData();
         new Thread(new Runnable() {
             @Override
             public void run() {
@@ -58,6 +62,8 @@ public class DataSource {
                         .setDescription(androidCentral.channelDescription)
                         .insert(writableDatabase);
 
+                List<RssItem> newRSSItems = new ArrayList<RssItem>();
+
                 for (GetFeedsNetworkRequest.ItemResponse itemResponse : androidCentral.channelItems) {
 
                     long itemPubDate = System.currentTimeMillis();
@@ -68,7 +74,7 @@ public class DataSource {
                         e.printStackTrace();
                     }
 
-                    new RssItemTable.Builder()
+                    long newItemRowId = new RssItemTable.Builder()
                             .setTitle(itemResponse.itemTitle)
                             .setDescription(itemResponse.itemDescription)
                             .setEnclosure(itemResponse.itemEnclosureURL)
@@ -78,7 +84,23 @@ public class DataSource {
                             .setPubDate(itemPubDate)
                             .setRSSFeed(androidCentralFeedId)
                             .insert(writableDatabase);
+
+                    Cursor itemCursor = rssItemTable.fetchRow(databaseOpenHelper.getReadableDatabase(), newItemRowId);
+                    itemCursor.moveToFirst();
+                    RssItem newRssItem = itemFromCursor(itemCursor);
+                    newRSSItems.add(newRssItem);
+                    itemCursor.close();
+
                 }
+
+                Cursor androidCentralCursor = rssFeedTable.fetchRow(databaseOpenHelper.getReadableDatabase(), androidCentralFeedId);
+                androidCentralCursor.moveToFirst();
+                RssFeed androidCentralRSSFeed = feedFromCursor(androidCentralCursor);
+                androidCentralCursor.close();
+                items.addAll(newRSSItems);
+                feeds.add(androidCentralRSSFeed);
+
+                BloclyApplication.getSharedInstance().sendBroadcast(new Intent(ACTION_DOWNLOAD_COMPLETED));
             }
         }).start();
     }
@@ -89,6 +111,19 @@ public class DataSource {
 
     public List<RssItem> getItems() {
         return items;
+    }
+
+    static RssFeed feedFromCursor(Cursor cursor) {
+        return new RssFeed(RssFeedTable.getTitle(cursor), RssFeedTable.getDescription(cursor),
+                RssFeedTable.getSiteURL(cursor), RssFeedTable.getFeedURL(cursor));
+    }
+
+    static RssItem itemFromCursor(Cursor cursor) {
+        return new RssItem(RssItemTable.getGUID(cursor), RssItemTable.getTitle(cursor),
+                RssItemTable.getDescription(cursor), RssItemTable.getLink(cursor),
+                RssItemTable.getEnclosure(cursor), RssItemTable.getRssFeedId(cursor),
+                RssItemTable.getPubDate(cursor), RssItemTable.getFavorite(cursor),
+                RssItemTable.getArchived(cursor));
     }
 
     void createFakeData() {
