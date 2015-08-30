4cab8ea158796e46b428f24a4fca4468fc7cf54f
i-zor066 <igor.zor@gmail.com>
2015-08-30 11:49:45 +0200

    Assignment 53 completed

--- Blocly/app/src/main/java/io/bloc/android/blocly/api/DataSource.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/api/DataSource.java
@@ -1,9 +1,15 @@
 package io.bloc.android.blocly.api;
 
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.BuildConfig;
@@ -40,8 +46,86 @@ public class DataSource {
                     BloclyApplication.getSharedInstance().deleteDatabase("blocly_db");
                 }
                 SQLiteDatabase writableDatabase = databaseOpenHelper.getWritableDatabase();
-                new GetFeedsNetworkRequest("http://feeds.feedburner.com/androidcentral?format=xml").performRequest();
+                List<GetFeedsNetworkRequest.FeedResponse> responseFeeds =  new GetFeedsNetworkRequest("http://feeds.feedburner.com/androidcentral?format=xml").performRequest();
+
+                for (GetFeedsNetworkRequest.FeedResponse feedResponse : responseFeeds) {
+
+                    String feedTitle = feedResponse.channelTitle;
+
+                    Cursor cursor = writableDatabase.rawQuery("SELECT * FROM rss_feeds WHERE title ='" + feedTitle + "'", null);
+
+                    if (cursor.getCount() <= 0) {
+                        String feedDescription = feedResponse.channelDescription;
+                        String channelURL = feedResponse.channelURL;
+                        String feedUrlString = feedResponse.channelFeedURL;
+
+                        ContentValues values = new ContentValues();
+
+                        values.put("link", channelURL);
+                        values.put("title", feedTitle);
+                        values.put("description", feedDescription);
+                        values.put("feed_url", feedUrlString);
+
+                        writableDatabase.insert("rss_feeds", null, values );
+                    }
+
+                    cursor.close();
+
+                    List<GetFeedsNetworkRequest.ItemResponse> channelItems = feedResponse.channelItems;
+
+                    for (GetFeedsNetworkRequest.ItemResponse itemResponse : channelItems) {
+
+                        String guid = itemResponse.itemGUID;
+
+                        Cursor cursor2 = writableDatabase.rawQuery("SELECT * FROM rss_items WHERE guid ='" + guid + "'", null);
+
+                        if (cursor2.getCount() <= 0) {
+
+
+                            String title = itemResponse.itemTitle;
+                            String description = itemResponse.itemDescription;
+                            String url = itemResponse.itemURL;
+                            String imageUrl = itemResponse.itemEnclosureURL;
+                            String itemEnclosureMIMEType = itemResponse.itemEnclosureMIMEType;
+                            long rssFeedId = 0;
+
+                            long datePublished = System.currentTimeMillis();
+                            DateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy kk:mm:ss z", Locale.ENGLISH);
+                            try {
+                                datePublished = dateFormat.parse(itemResponse.itemPubDate).getTime();
+                            } catch (ParseException e) {
+                                e.printStackTrace();
+                            }
+
+                            boolean favorite = false;
+                            boolean archived = false;
+
+                            ContentValues values = new ContentValues();
+
+                            values.put("link", url);
+                            values.put("title", title);
+                            values.put("description", description);
+                            values.put("guid", guid);
+                            values.put("pub_date", datePublished);
+                            values.put("enclosure", imageUrl);
+                            values.put("mime_type", itemEnclosureMIMEType);
+                            values.put("rss_feed", rssFeedId);
+                            values.put("is_favorite", favorite);
+                            values.put("is_archived", archived);
+
+                            writableDatabase.insert("rss_items", null, values);
+                        }
+
+                        cursor2.close();
+
+                    }
+
+                }
+
+
             }
+
+
         }).start();
     }
 
@@ -52,6 +136,8 @@ public class DataSource {
     public List<RssItem> getItems() {
         return items;
     }
+
+
 
     void createFakeData() {
         feeds.add(new RssFeed("My Favorite Feed",
@@ -66,4 +152,6 @@ public class DataSource {
                     0, System.currentTimeMillis(), false, false));
         }
     }
+
+
 }
