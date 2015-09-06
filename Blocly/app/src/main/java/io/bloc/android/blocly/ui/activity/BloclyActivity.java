25e8a43568cebba85897cf1d9cca2b0827078c4f
i-zor066 <igor.zor@gmail.com>
2015-09-06 19:50:49 +0200

    Checkpoint 59 completed

--- Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ Blocly/app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -28,6 +28,7 @@ import io.bloc.android.blocly.api.DataSource;
 import io.bloc.android.blocly.api.model.RssFeed;
 import io.bloc.android.blocly.api.model.RssItem;
 import io.bloc.android.blocly.ui.adapter.NavigationDrawerAdapter;
+import io.bloc.android.blocly.ui.fragment.RssItemDetailFragment;
 import io.bloc.android.blocly.ui.fragment.RssItemListFragment;
 
 /**
@@ -47,6 +48,7 @@ public class BloclyActivity extends ActionBarActivity implements
     private List<RssFeed> allFeeds = new ArrayList<RssFeed>();
 
     private RssItem expandedItem = null;
+    private boolean onTablet;
 
 
 
@@ -54,6 +56,8 @@ public class BloclyActivity extends ActionBarActivity implements
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_blocly);
+
+        onTablet = findViewById(R.id.fl_activity_blocly_right_pane) != null;
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.tb_activity_blocly);
         setSupportActionBar(toolbar);
@@ -239,6 +243,13 @@ public class BloclyActivity extends ActionBarActivity implements
     @Override
     public void onItemExpanded(RssItemListFragment rssItemListFragment, RssItem rssItem) {
         expandedItem = rssItem;
+        if (onTablet) {
+            getFragmentManager().beginTransaction()
+                    .replace(R.id.fl_activity_blocly_right_pane, RssItemDetailFragment.detailFragmentForRssItem(rssItem))
+                    .commit();
+
+            return;
+        }
         animateShareItem(expandedItem != null);
     }
 
