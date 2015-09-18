16b7c404a040806697e47737625186aad8c09c2b
i-zor066 <igor.zor@gmail.com>
2015-09-18 23:41:35 +0100

    Start

---
+++ Blocly/app/src/androidTest/java/io/bloc/android/blocly/ui/activity/ActivityTest.java
@@ -0,0 +1,13 @@
+package io.bloc.android.blocly.ui.activity;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+/**
+ * Created by igor on 18/9/15.
+ */
+public class ActivityTest extends ActivityInstrumentationTestCase2<BloclyActivity> {
+
+    public ActivityTest() {
+        super(BloclyActivity.class);
+    }
+}
